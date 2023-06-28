ee975cd79cadafd6922c6511e758a7341a5454c3
jay dobariya <jaydobariya2341@gmail.com>
2023-06-28 22:48:31 +0530

    day-4 done

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -21,6 +21,33 @@ class LoginPage extends StatelessWidget {
             SizedBox(
               height: 2.0,
             ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 45.0),
+              child: Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                        hintText: "Enter UserName", labelText: "UserName"),
+                  ),
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        hintText: "Enter UserName", labelText: "UserName"),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      print("login succesful");
+                    },
+                    child: Text("Login"),
+                    style: TextButton.styleFrom(),
+                  )
+                ],
+              ),
+            )
           ],
         ));
   }
