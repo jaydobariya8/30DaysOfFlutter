7f0ad336a1b10619bf82292ac874dfd682443f8b
jay dobariya <jaydobariya2341@gmail.com>
2023-07-10 22:06:11 +0530

    day-9 done

---
+++ lib/widgets/drawer.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final imgUrl =
+        "https://yt3.googleusercontent.com/ytc/AL5GRJW00GdUXWSu0Cf8-HyzNo1_ZsDplW_6CZfpLBxpxg=s900-c-k-c0x00ffffff-no-rj";
+    return Drawer(
+      child: Container(
+        color: Colors.deepPurple,
+        child: ListView(
+          children: [
+            DrawerHeader(
+                padding: EdgeInsets.zero,
+                child: UserAccountsDrawerHeader(
+                  decoration: BoxDecoration(color: Colors.grey),
+                  margin: EdgeInsets.zero,
+                  accountName: Text("Jay dobariya"),
+                  accountEmail: Text("jaydobariya2341@gmail.com"),
+                  currentAccountPicture: Image.network(imgUrl),
+                )),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.home,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Home",
+                style: TextStyle(color: Colors.white),
+                textScaleFactor: 1.2,
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.profile_circled,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Profile",
+                style: TextStyle(color: Colors.white),
+                textScaleFactor: 1.2,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
